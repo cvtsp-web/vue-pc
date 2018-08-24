7a23dd22f79c221d53c2b906420ee13caa8f8b98
gaoyanan <1440374646@qq.com>
2018-08-24 09:00:19 +0800

    api

--- src/components/DevHelper/src/DevHelper-chooseapi.vue
+++ src/components/DevHelper/src/DevHelper-chooseapi.vue
@@ -1,8 +1,10 @@
 <template>
     <el-select v-model="currentAPI" style="width: 100%">
-        <el-option v-for="(opt, index) in apiOptions" :key="index">
-
-        </el-option>
+        <el-option 
+        v-for="(opt, index) in apiOptions" 
+        :key="index" 
+        :label="getLabel(opt)" 
+        :value="opt.api" />
     </el-select>
 </template>
 
@@ -11,14 +13,23 @@ export default {
     data() {
         return{
             currentAPI: '',
+            // { api: String, name: String}
             apiOptions: []
         }
     },
     inject: ['actions'],
     async mounted() {
-        const results = await this.actions.findDevApi();
-        console.log(results)
-    }
+        const { flag, data } = await this.actions.findDevApi();
+        
+        if(flag) {
+            this.apiOptions = data;
+        }
+    },
+    methods: {
+        getLabel(opt) {
+            return `${opt.name}  api: ${opt.api}`
+        },
+    },
 }
 </script>
 
